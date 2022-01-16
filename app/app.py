39c76eb4b639afb00173b4e5217635594ce9b293
Ivan <ivan.jump@globo.com>
2022-01-16 15:05:23 -0300

    feature: teste, upload with messagen error 1mb+, download, get /files arquivo env

---
+++ app/app.py
@@ -0,0 +1,80 @@
+from dotenv.main import load_dotenv
+from flask import Flask,request,send_from_directory
+from kenzie import upload,get_files_filtred,get_name_files,create_zip_by_extension
+
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+app.config['MAX_CONTENT_LENGTH'] = int(os.getenv('MAX_CONTENT_LENGTH'))
+
+base_dir = os.getenv('FILES_DIRECTORY')
+extension_list = os.getenv('ALLOWED_EXTENSIONS').split(',')
+
+try:
+    os.mkdir(f'{base_dir}')
+    for extension in extension_list:
+        try:
+            os.mkdir(f'{base_dir}/{extension}')
+        except:
+            pass
+except:
+    pass
+
+
+@app.get('/')
+def home():
+    return{'mensagem':'ta na home'},200
+
+@app.post('/upload')
+def file_upload():
+    file = request.files['file']
+    (mensagem,status) = upload(file)
+    return {'mensagem': mensagem},status
+
+@app.get('/files')
+def file_listed():
+    return {'files': get_name_files()},200
+
+@app.get('/files/<extension>')
+def files_filtred(extension):
+    if extension in extension_list:
+        list_files = get_files_filtred(extension)
+        return {'files': list_files},200
+    return {'mensagem':'Extensão invalida'},404
+
+@app.get('/download/<file_name>')
+def download_file(file_name):
+    extension = file_name.split('.')[1]
+    try:
+        return send_from_directory(
+            directory= f'{base_dir}/{extension}/',
+            path= file_name,
+            as_attachment=True
+        )
+    except:
+        return  {'mensagem':'Nome de arquivo inválido'},404
+
+@app.get('/download-zip')
+def download_zip():
+    try:
+        extension = request.args.get('file_extension')
+        radios = int(request.args.get('compression_ratio'))
+        
+        response = create_zip_by_extension(extension,radios)
+        if response:
+            file_download = send_from_directory(
+                directory= f'{base_dir}/{extension}/',
+                path= response,
+                as_attachment=True
+            )
+            os.system(f'rm /tmp/files_upload/{extension}/{extension}.zip')
+            return file_download
+        return {'mensagem':'pasta vazia'},404
+    except:
+        mensagem = 'file_extension e compression_ratio necessarios'
+        if extension not in extension_list:
+            mensagem ='aplicasão não suporta essa extensão'
+        return {'mensagem': mensagem},404
---
+++ kenzie/imagens.py
@@ -0,0 +1,54 @@
+import os
+
+def get_name_files():
+    base_dir = os.getenv('FILES_DIRECTORY')
+    extension_list = list(os.getenv('ALLOWED_EXTENSIONS').split(','))
+    list_files = []
+    for extension in extension_list:
+        extension_dir = f'{base_dir}/{extension}' 
+        for _,_,files in os.walk(extension_dir):
+            list_files.append(files)
+    return sum(list_files,[])
+
+def get_files_filtred(extension_filter):
+    files = get_name_files()
+    files_filtred = [file for file in files if extension_filter in file]
+    return files_filtred
+
+def save_file(file,extension):
+    base_dir = os.getenv('FILES_DIRECTORY')
+    extension_dir = f'{base_dir}/{extension}'
+    file_name = file.filename
+    list_files = get_name_files()
+
+    if file_name in list_files:
+        return ('Imagem com um nome ja existente no sistema,',409)
+    else:
+        file.save(f'{extension_dir}/{file.filename}')
+        return ('Imagen salva',201)
+        
+def upload(file):
+    max_size = int(os.getenv('MAX_CONTENT_LENGTH'))
+    extension_list = os.getenv('ALLOWED_EXTENSIONS').split(',')
+    extension = file.filename.split('.')[-1]
+    
+    if extension in extension_list:
+        file_length = len(file.read())
+        file.seek(0)
+        if file_length<=max_size:
+            mensagem = save_file(file,extension)
+            return mensagem
+
+        return (f'Arquivo maior que {max_size/1000000} MB', 413)
+        
+    return (f'Extensão .{extension} não é suportada', 415)
+
+
+def create_zip_by_extension(extension,radios):
+    base_dir = os.getenv('FILES_DIRECTORY')
+    extension_dir = f'{base_dir}/{extension}'
+    files = ''.join(os.listdir(f'{base_dir}/{extension}'))
+    if files:
+        os.system(f'cd {extension_dir} ; zip -{radios} {extension}.zip *.{extension}')
+        return f'{extension}.zip'
+    return ''
